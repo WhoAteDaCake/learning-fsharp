a6dcdfa8d1723ff27f38f0a7de59798c555d2f04
WhoAteDaCake <doitsimplefy@gmail.com>
2022-10-28 21:44:21 +0100

    chore: lint

--- src/Client/Routing.fs
+++ src/Client/Routing.fs
@@ -1,20 +1,33 @@
 module Client.Routing
 
 module Routes =
-    let bookmarks = "bookmarks"
-    let home = "home"
+    [<Literal>]
+    let Bookmarks = "bookmarks"
+
+    [<Literal>]
+    let Home = "home"
+
+    [<Literal>]
+    let NotFound = "404"
 
 [<RequireQualifiedAccess>]
 type Url =
     | Home
+    | Bookmarks
     | NotFound
 
 let parseUrl =
     function
     | [] -> Url.Home
+    | [ Routes.Bookmarks ] -> Url.Home
     | _ -> Url.NotFound
 
-let topLevelRoutes = [
-    (Url.Home, "/home", "Home");
-    (Url.Home, "/home", "Home");
-]
+let urlToPath =
+    function
+    | Url.Home -> "/" + Routes.Home
+    | Url.Bookmarks -> "/" + Routes.Bookmarks
+    | Url.NotFound -> "/" + Routes.NotFound
+
+let topLevelRoutes =
+    [ (Url.Home, "Home")
+      (Url.Bookmarks, "Bookmarks") ]
