712a12b595c41892fb0244ed82e5c520383712c9
WhoAteDaCake <doitsimplefy@gmail.com>
2022-11-25 09:08:14 +0000

    feat: separate out tree views

--- src/Client/Pages/Bookmarks.fs
+++ src/Client/Pages/Bookmarks.fs
@@ -9,35 +9,54 @@ open Feliz
 open Feliz.AntdReact
 open Fable.Core
 
+type Leaf = {
+    Id: string
+    Title: string
+    ParentId: string option
+    Icon: string
+    Url: string;
+}
+type Branch = {
+    Id: string
+    ParentId: string option
+    Title: string
+    Children: Tree list
+}
+and Tree =
+| TBranch of Branch
+| TLeaf of Leaf
+
 type Model =
-    { Bookmarks: Deferred<Result<TreeData, string>> }
+    { Bookmarks: Deferred<Result<Tree, string>> }
 
 type Msg =
-    | Append of TreeData
-    | Load of AsyncOperationStatus<Result<TreeData, string>>
+    | Append of Tree
+    | Load of AsyncOperationStatus<Result<Tree, string>>
 
-let fakeGraph: TreeData =
-    { title = U2.Case1 (Html.text "parent 1")
-      key = U2.Case1 "0-0"
-      icon = None
-      disabled = false
-      selectable = true
-      children =
-        [| { title = U2.Case1 (Html.text "parent 1-0")
-             key = U2.Case1 "0-0-0"
-             disabled = true
-             icon = None
-             selectable = true
-             children =
-               [| { title = U2.Case1 (Html.text "leaf")
-                    key = U2.Case1 "0-0-0-0"
-                    icon = None
-                    disabled = false
-                    selectable = true
-                    children = [||] } |] } |] }
+let fakeData: Tree = TBranch {
+    Id = "0";
+    Title = "Bookmarks bar"
+    ParentId = None
+    Children = [
+        TBranch {
+            Id = "1"
+            Title = "Searches"
+            ParentId = Some "0"
+            Children = [
+                TLeaf {
+                    Id = "2"
+                    ParentId = Some "1"
+                    Title = "Previous google search"
+                    Icon = "test"
+                    Url = "http://google.com"
+                }
+            ]
+        }
+    ]
+}
 
 let fakeAsyncLoad () =
-    async { return AsyncOperationStatus.Finished(Result.Ok fakeGraph) }
+    async { return AsyncOperationStatus.Finished(Result.Ok fakeData) }
 
 let init () : Model * Cmd<Msg> =
     let model = { Bookmarks = HasNotStartedYet }
@@ -57,12 +76,37 @@ let update (msg: Msg) (model: Model) : Model * Cmd<Msg> =
     | Load (Finished result) -> { model with Bookmarks = Resolved result }, Cmd.none
     | _ -> model, Cmd.none
 
+let rec renderTree = function
+| TBranch branch ->
+    let output: TreeData = {
+        title = U2.Case1 (Html.text branch.Title)
+        key = U2.Case1 branch.Id
+        icon = None
+        disabled = false
+        selectable = true
+        children =
+            branch.Children
+            |> (List.map renderTree)
+            |> Array.ofSeq
+    }
+    output
+| TLeaf leaf ->
+    let output: TreeData = {
+        title = U2.Case1 (Html.text leaf.Title)
+        key = U2.Case1 leaf.Id
+        icon = None
+        disabled = false
+        selectable = true
+        children = [||]
+    }
+    output
+
 let view (model: Model) (dispatch: Msg -> unit) =
     let bookmarks =
         match model.Bookmarks with
         | Resolved (Ok result) ->
             Antd.tree [
-                tree.treeData [result]
+                tree.treeData [renderTree result]
                 tree.defaultExpandAll true
             ]
         | _ -> Html.text "Failed to load"
