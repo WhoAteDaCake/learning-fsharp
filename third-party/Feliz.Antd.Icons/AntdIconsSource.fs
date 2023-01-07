9e7a06ac6bc16b2811678f7ae21dfc6f888e1e1a
WhoAteDaCake <doitsimplefy@gmail.com>
2023-01-07 10:20:20 +0000

    feat: icons library

---
+++ third-party/Feliz.Antd.Icons/AntdIconsSource.fs
@@ -0,0 +1,36 @@
+﻿namespace Generated.Feliz
+
+open System
+open Browser.Types
+open Myriad.Plugins
+open Fable.Core.JsInterop
+open Fable.Core
+open Feliz
+
+[<Generator.Methods>]
+type WithStyle =
+    static member inline style(properties: #IStyleAttribute seq) =
+        Interop.attr "style" (createObj !!(properties |> Array.ofSeq))
+
+[<Generator.Methods>]
+type WithClass =
+    static member inline className(value: string) = Interop.attr "className" value
+
+    static member inline className(names: seq<string>) =
+        Interop.attr "className" (String.concat " " names)
+
+
+[<Generator.ModuleRoot>]
+module rec AntdIconsReact =
+    [<Generator.Component; Generator.ExtendsMethods(typeof<WithStyle>, typeof<WithClass>)>]
+    type folderOutlined() =
+        static member inline create(properties: Interop.inlined seq) =
+            Interop.reactApi.createElement (import "FolderOutlined" "@ant-design/icons", createObj !!(Array.ofSeq properties))
+
+        static member inline disabled(value: bool) = Interop.attr "disabled" value
+        static member inline label(value: string) = Interop.attr "label" value
+
+[<Generator.LibraryRoot>]
+type AntdIcons =
+    class
+    end
